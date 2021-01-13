3b81f18e964f5def56f8f10d5d8d09b0832026ec
itay-cyber <itaykag@gmail.com>
2021-01-13 13:01:21 +0100

    Revert "Made animations code"
    This reverts commit c91ed70f8ee2bc4bf3e5dfb547027a5210d639ba.

--- Crashed-Final-Final/Assets/Scripts/PlayerMovement.cs
+++ Crashed-Final-Final/Assets/Scripts/PlayerMovement.cs
@@ -4,52 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Animator animator;
-    public GameObject playerObj;
-
-    //bools
-
-    public bool isWalking;
-    public bool forwardPressed;
-    public bool runPressed;
-    public int isWalkingHash;
-    public bool isRunning;
-    public int isRunningHash;
-
     void Start()
     {
-        animator = playerObj.GetComponent<Animator>();
-        isWalkingHash = Animator.StringToHash("isWalking");
-        isRunningHash = Animator.StringToHash("isRunning");
-
-
+        
     }
 
     void Update()
     {
-        forwardPressed = Input.GetKey("w");
-        runPressed = Input.GetKey("left shift");
-
-        isRunning = animator.GetBool(isRunningHash);
-        isWalking = animator.GetBool(isWalkingHash);
-
-        if (!isWalking && forwardPressed)
-        {
-            animator.SetBool(isWalkingHash, true);
-        }
-
-        if (isWalking && !forwardPressed)
-        {
-            animator.SetBool(isWalkingHash, false);
-        }
-
-        if (!isRunning && (forwardPressed && runPressed))
-        {
-            animator.SetBool(isRunningHash, true);
-        }
-        if (isRunning && (!forwardPressed || !runPressed))
-        {
-            animator.SetBool(isRunningHash, false);
-        }
+        
     }
 }
