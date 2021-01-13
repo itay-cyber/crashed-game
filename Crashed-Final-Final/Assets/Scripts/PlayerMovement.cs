3c045b8874c2c735956eecfb2f1a573d41e1a2a3
itay-cyber <itaykag@gmail.com>
2021-01-13 14:44:17 +0100

    Dun stuff but no luck moving the player

--- Crashed-Final-Final/Assets/Scripts/PlayerMovement.cs
+++ Crashed-Final-Final/Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public GameObject playerObj;
     public Animator playerAnimator;
+    public Rigidbody rb;
 
     //bools
     public bool isRunning;
     public bool isWalking;
     public bool runPressed;
     public bool forwardPressed;
-
+    
     //hash
     private int isWalkingHash;
     private int isRunningHash;
     
     void Start()
     {
+        rb = playerObj.GetComponent<Rigidbody>();
         playerAnimator = playerObj.GetComponent<Animator>();
 
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+
     }
 
     void Update()
@@ -33,21 +36,25 @@ public class PlayerMovement : MonoBehaviour
         forwardPressed = Input.GetKey("w");
         runPressed = Input.GetKey("left shift");
 
+        //walk forward
         if (!isWalking && forwardPressed)
         {
             playerAnimator.SetBool(isWalkingHash, true);
-        }
+        } 
 
+        //stop walking
         if (isWalking && !forwardPressed)
         {
             playerAnimator.SetBool(isWalkingHash, false);
         }
 
+        //run forward
         if (!isRunning && (forwardPressed && runPressed))
         {
             playerAnimator.SetBool(isRunningHash, true);
         }
 
+        //stop running
         if (isRunning && (!forwardPressed || !runPressed))
         {
             playerAnimator.SetBool(isRunningHash, false);
