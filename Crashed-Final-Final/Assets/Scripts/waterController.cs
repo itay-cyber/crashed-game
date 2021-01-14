06fa9ced02fa569d4af777827ffb0275f7990f1a
ccm7676 <carlcmarino@gmail.com>
2021-01-14 18:31:33 +0100

    god has created water

---
+++ Crashed-Final-Final/Assets/Scripts/waterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class waterController : MonoBehaviour
+{
+    public float water = 100f;
+    public Image waterImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waterImg.rectTransform.sizeDelta = new Vector2(100f,94.21179f/100 * water);
+    }
+}
