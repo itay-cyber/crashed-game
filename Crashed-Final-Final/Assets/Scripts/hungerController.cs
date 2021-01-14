2fb5ae9f479a0b9447afdc5c7fda750187cba6c3
ccm7676 <carlcmarino@gmail.com>
2021-01-14 17:12:32 +0100

    hunger ui done

---
+++ Crashed-Final-Final/Assets/Scripts/hungerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hungerController : MonoBehaviour
+{
+    public float hunger = 100f;
+    public Image hungerImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hungerImg.rectTransform.sizeDelta = new Vector2(100f, 100f/100 * hunger); 
+    }
+}
