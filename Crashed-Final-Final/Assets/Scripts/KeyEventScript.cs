b307a0b3e183274d535c6b8ca31e4a0abb8bb5d7
ccm7676 <carlcmarino@gmail.com>
2021-01-25 21:06:26 +0100

    Added inventory E to open and made ui adaptive(but aspect ratio hasto be 16:9

---
+++ Crashed-Final-Final/Assets/Scripts/KeyEventScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyEventScript : MonoBehaviour
+{
+    public CanvasGroup inventory;
+    public bool inventoryVisible = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            print("hello");
+            if (inventoryVisible == true)
+            {
+                inventory.alpha = 0;
+                inventory.interactable = false;
+                inventoryVisible = false;
+                Cursor.visible = false;
+                Screen.lockCursor = true;
+            }
+
+            else
+            {
+                inventory.alpha = 1;
+                inventory.interactable = true;
+                inventoryVisible = true;
+                Cursor.visible = true;
+                Screen.lockCursor = false;
+            }
+
+
+        }
+    }
+}
